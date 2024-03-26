e01301a4adc031246a0776c38f5e1c79140f2ce4
dami <87931528+Damijongerius@users.noreply.github.com>
2024-03-26 23:46:58 +0100

    RoadMesh

--- Assets/Fase1/Scripts/MeshComponents/RoadComponent.cs
+++ Assets/Fase1/Scripts/MeshComponents/RoadComponent.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Rendering.HighDefinition;
 using Random = UnityEngine.Random;
 
 namespace Fase1.MeshComponents
@@ -37,8 +35,75 @@ namespace Fase1.MeshComponents
 
         public MeshComponentData[] GenerateMeshData(Vector2Int chunkPosition, int verticesCount, float physicalSize)
         {
+            float[,] noise = _noiseGenerator.GenerateNoiseChunk(chunkPosition.x, chunkPosition.y);
+
+            Vector2 startChunk = new Vector2(chunkPosition.x * physicalSize, chunkPosition.y * physicalSize);
+            Vector2 endChunk = new Vector2((chunkPosition.x + 1) * physicalSize, (chunkPosition.y + 1) * physicalSize);
+
+            List<Vector2> positions = new List<Vector2>();
             
-            return null;
+            foreach (var pos in _mainPositions)
+            {
+                if (pos.x > startChunk.x && pos.x < endChunk.x && pos.y > startChunk.y && pos.y < endChunk.y)
+                {
+                    positions.Add(pos);
+                }
+            }
+            
+            List<Vector2> spline = RenderSpline(positions);
+            
+            List<int> triangles = new List<int>();
+            List<Vector3> vertices = new List<Vector3>();
+            List<Vector2> uvs = new List<Vector2>();
+            
+            //calculate the vertices ,triangles and uvs
+            for (int x = 0; x < verticesCount - 1; x++)
+            {
+                for (int y = 0; y < verticesCount - 1 ; y++)
+                {
+                    Vector3[] v = GetVertices(x,y);
+                    Vector2[] uv = GetUvs();
+                    
+                    for (int k = 0; k < 6; k++)
+                    {
+                        vertices.Add(v[k]);
+                        triangles.Add(vertices.Count - 1);
+                        uvs.Add(uv[k]);
+                    }
+
+                }
+            }
+            
+            Vector3[] GetVertices(int x, int y)
+            {
+                
+                //each corner of the quad
+                Vector3 a = new Vector3(x, noise[x, y], y);
+                Vector3 b = new Vector3((x + 1), noise[x + 1, y], y);
+                Vector3 c = new Vector3(x, noise[x, y + 1], (y + 1));
+                Vector3 d = new Vector3((x + 1), noise[x + 1, y + 1], (y + 1));
+                
+                return new[] { a, c, d, a, d, b };
+            }
+
+            Vector2[] GetUvs()
+            {
+                //corners of the UVs
+                Vector2 uv00 = new Vector2(0f, 0f);
+                Vector2 uv10 = new Vector2(1f, 0f);
+                Vector2 uv01 = new Vector2(1f, 1f);
+                Vector2 uv11 = new Vector2(1f, 1f);
+                
+                return new[] { uv00, uv10, uv01, uv10, uv11, uv01 };
+            }
+
+
+
+            //hardcoded the material index 0-0
+            Dictionary<int, List<int>> combinedTriangles = new Dictionary<int, List<int>> { { 1, triangles } };
+
+            return new[] { new MeshComponentData(combinedTriangles, vertices, uvs) };
+
         }
 
         private void UpdateSplinePositions()
