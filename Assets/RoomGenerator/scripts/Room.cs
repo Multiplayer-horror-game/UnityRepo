65a77eae4a0ff3b540a473cb8cb00c94d8628ca9
damii ii ii <87931528+Damijongerius@users.noreply.github.com>
2024-04-11 09:02:46 +0200

    room gen
    leaving room for hallways

--- Assets/RoomGenerator/scripts/Room.cs
+++ Assets/RoomGenerator/scripts/Room.cs
@@ -1,14 +1,19 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Room : MonoBehaviour
+namespace RoomGenerator.scripts
 {
-    public List<Vector2Int> positions;
-    
-    void Start()
+    public class Room : MonoBehaviour
     {
-        
+        public List<Vector2Int> positions;
+        public List<Door> doors;
+        public List<Vector2Int> hallwayPositions;
+    
     }
     
+    public struct Door
+    {
+        public Vector2Int position;
+        public Vector2Int direction;
+    }
 }
--- Assets/RoomGenerator/scripts/RoomManager.cs
+++ Assets/RoomGenerator/scripts/RoomManager.cs
@@ -1,108 +1,186 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class RoomManager : MonoBehaviour
+namespace RoomGenerator.scripts
 {
-    //posible rooms to spawn
-    public List<GameObject> rooms;
-
-    //size of the world grid
-    public Vector2Int gridSize = new Vector2Int(10, 10);
-
-    //the amount of rooms you want to generate
-    public int preferdRooms = 5;
-    
-    //room and offset
-    private Dictionary<Room,Vector2> usedRooms = new ();
-
-    //final grid layout (not needed for anything rn)
-    private bool[,] bakedGrid;
-
-    public int scale;
-    void Start()
+    public class RoomManager : MonoBehaviour
     {
-        bakedGrid = new bool[gridSize.x,gridSize.y];
 
-        bool noSpace = false;
-        
-        int roomsGenerated = 0;
-        while (!noSpace && roomsGenerated != preferdRooms)
+        //size of the world grid
+        public Vector2Int gridSize = new Vector2Int(10, 10);
+    
+        //room and offset
+        private Dictionary<Room,Vector2> usedRooms = new ();
+    
+
+        public RoomProperties[] rooms;
+    
+        public HallwayProperties[] hallways;
+
+        //final grid layout 
+        //0 - empty
+        //1 - room
+        //2 - hallway
+        private int[,] bakedGrid;
+
+        public int scale;
+
+        void Start()
         {
-            int x = Random.Range(0, gridSize.x);
-            int y = Random.Range(0, gridSize.y);
+            bakedGrid = new int[gridSize.x, gridSize.y];
 
-            if (!bakedGrid[x, y])
+            RoomProperties[] mandatoryRooms = Array.FindAll(rooms, room => room.mandatory);
+
+            foreach (var room in mandatoryRooms)
             {
-                
-                Debug.Log((x) + "," + (y));
+                int x = Random.Range(0, gridSize.x);
+                int y = Random.Range(0, gridSize.y);
 
-                int roomNumber = Random.Range(0,rooms.Count);
+                int failsafe = 0;
                 
-                GameObject instantiatedRoom = Instantiate(rooms[roomNumber], new Vector3(x * scale,0,y * scale),new Quaternion());
+                while (!CheckPositions(room.room.GetComponent<Room>(), new Vector2Int(x, y)) && failsafe < 100)
+                {
+                    x = Random.Range(0, gridSize.x);
+                    y = Random.Range(0, gridSize.y);
+                    failsafe++;
+                }
+                
+                if (failsafe >= 100)
+                {
+                    Debug.LogError("Could not place mandatory room in 100 tries. Exiting.");
+                    return;
+                }
+                
+                SetGridPositions(room.room.GetComponent<Room>(), new Vector2Int(x, y));
+                
+                GameObject instantiatedRoom = Instantiate(room.room, new Vector3(x,0,y),new Quaternion());
                 Room roomClass = instantiatedRoom.GetComponent<Room>();
                 
-                foreach (Vector2Int pos in roomClass.positions)
+                List<Vector2Int> realizedPositions = new List<Vector2Int>();
+                
+                foreach (Vector2Int roomClassPosition in roomClass.positions)
                 {
-                    if (!(pos.x + x < gridSize.x && pos.y + y < gridSize.y) || !(pos.x + x >= 0 && pos.y + y >= 0))
-                    {
-                        DestroyRoom();
-                        break;
-                    }
-                    
-                    if(bakedGrid[pos.x + x, pos.y + y])
-                    {
-                        DestroyRoom();
-                        break;
-                    }
-                    
-                    bakedGrid[pos.x + x, pos.y + y] = true;
+                    realizedPositions.Add(new Vector2Int(roomClassPosition.x + x, roomClassPosition.y + y));
                 }
+                
+                roomClass.positions = realizedPositions;
+                
+            }
+            
+        }
 
-                usedRooms.Add(roomClass, new Vector2(x,y));
-                roomsGenerated++;
-
-                void DestroyRoom()
+        private bool CheckPositions(Room room, Vector2Int position)
+        {
+            foreach (Vector2Int pos in room.positions)
+            {
+                if (!(pos.x + position.x < gridSize.x && pos.y + position.y < gridSize.y) || !(pos.x + position.x >= 0 && pos.y + position.y >= 0))
                 {
-                    Destroy(instantiatedRoom);
+                    Debug.Log("Room out of bounds at " + position);
+                    return false;
+                }
+                
+                if(bakedGrid[pos.x + position.x,pos.y + position.y] != 0)
+                {
+                    Debug.Log("Room overlaps at " + position);
+                    return false;
+                }
+            }
 
-                    foreach (Vector2Int pos2 in roomClass.positions)
+            Debug.Log("Room placed at " + position);
+            
+            return CheckHallwaySpace(room, position);
+        }
+
+        //checks that if the room is placed if it has space for an additional hallway
+        private bool CheckHallwaySpace(Room room, Vector2Int position)
+        {
+            foreach (Vector2Int pos in room.hallwayPositions)
+            {
+                if (pos.x + position.x < gridSize.x && pos.y + position.y < gridSize.y && pos.x + position.x >= 0 && pos.y + position.y >= 0)
+                {
+                    if (bakedGrid[pos.x + position.x, pos.y + position.y] == 0)
                     {
-                        if ((pos2.x + x < gridSize.x && pos2.y + y < gridSize.y) && (pos2.x + x >= 0 && pos2.y + y >= 0))
-                        {
-                            Debug.Log("Removing " + (pos2.x + x) + "," + (pos2.y + y));
-                            bakedGrid[pos2.x + x, pos2.y + y] = false;
-                        }
+                        return true;
                     }
                 }
             }
+            
+            return true;
         }
         
-        foreach (var room in usedRooms)
+        private void SetGridPositions(Room room, Vector2Int position)
         {
-             Debug.Log(new Vector3(room.Value.x,0,room.Value.y));
-        }
-    }
-    
-    private void OnDrawGizmos()
-    {
-        if (bakedGrid != null)
-        {
-            Gizmos.color = Color.green;
-            for (int x = 0; x < gridSize.x; x++)
+            foreach (Vector2Int pos in room.positions)
             {
-                for (int y = 0; y < gridSize.y; y++)
+                bakedGrid[pos.x + position.x,pos.y + position.y] = 1;
+            }
+            
+            SetGridHallwayPositions(room, position);
+        }
+        
+        private void SetGridHallwayPositions(Room room, Vector2Int position)
+        {
+            foreach (Vector2Int pos in room.hallwayPositions)
+            {
+                bakedGrid[pos.x + position.x,pos.y + position.y] = 2;
+            }
+        }
+    
+        private void OnDrawGizmos()
+        {
+            if (bakedGrid != null)
+            {
+                Gizmos.color = Color.green;
+                for (int x = 0; x < gridSize.x; x++)
                 {
-                    Vector3 center = new Vector3(x * scale, 0, y * scale);
-                    Gizmos.DrawWireCube(center, Vector3.one);
-                    if (bakedGrid[x, y])
+                    for (int y = 0; y < gridSize.y; y++)
                     {
-                        Gizmos.color = Color.red;
-                        Gizmos.DrawCube(center, Vector3.one * 0.8f);
+                        Vector3 center = new Vector3(x * scale, 0, y * scale);
+                        Gizmos.DrawWireCube(center, Vector3.one);
+                        switch (bakedGrid[x,y])
+                        {
+                            case 1:
+                                Gizmos.color = Color.red;
+                                Gizmos.DrawCube(center, Vector3.one);
+                                break;
+                            case 2:
+                                Gizmos.color = Color.blue;
+                                Gizmos.DrawCube(center, Vector3.one);
+                                break;
+                        }
                         Gizmos.color = Color.green;
+                        
                     }
                 }
             }
         }
+    }
+
+    [Serializable]
+    public struct RoomProperties
+    {
+        public bool mandatory;
+        public int maxAmount;
+        public GameObject room;
+    }
+    
+    [Serializable]
+    public struct HallwayProperties
+    {
+        public GameObject hallway;
+        
+        //north east south west
+        public Directions directions;
+    }
+
+    [Serializable]
+    public struct Directions
+    {
+        public bool north;
+        public bool east;
+        public bool south;
+        public bool west;
     }
 }
