359e2ad18510c6f10a3079a39bac589a7f2e1095
Tiana <tianastielowe@gmail.com>
2024-06-30 16:39:28 +0200

    jumpscare

--- Assets/Jumpscare.cs
+++ Assets/Jumpscare.cs
@@ -1,29 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class Jumpscare : MonoBehaviour
 {
+    public AudioSource Scream;
     public GameObject jumpscareObject;
+    public GameObject Player;
+    public GameObject FlashImg;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         jumpscareObject.SetActive(false);
+        FlashImg.SetActive(false);
     }
 
      void OnTriggerEnter(Collider player)
      {
         if(player.tag == "Player")
         {
+            Scream.Play();
             jumpscareObject.SetActive(true);
+            FlashImg.SetActive(true);
             StartCoroutine(DisableObject());
         }
      }
     IEnumerator DisableObject()
     {
-        yield return new WaitForSeconds(1.5f);
+        yield return new WaitForSeconds(2.5f);
         jumpscareObject.SetActive(false);
         gameObject.SetActive(false);
+        FlashImg.SetActive(false);
     }
 
     // Update is called once per frame
@@ -31,4 +42,6 @@ public class Jumpscare : MonoBehaviour
     {
         
     }
+
+  
 }
